34ac586f4f3a437954ffc66fb07ba01f87bbc789
Vladimir Popov <papoff8295@gmail.com>
2020-12-18 15:01:44 +0300

    11 12

--- src/main/java/ru/javawebinar/topjava/web/user/AdminUIController.java
+++ src/main/java/ru/javawebinar/topjava/web/user/AdminUIController.java
@@ -1,18 +1,10 @@
 package ru.javawebinar.topjava.web.user;
 
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.context.MessageSource;
-import org.springframework.context.i18n.LocaleContextHolder;
-import org.springframework.dao.DataIntegrityViolationException;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.MediaType;
-import org.springframework.http.ResponseEntity;
-import org.springframework.validation.BindingResult;
 import org.springframework.web.bind.annotation.*;
 import ru.javawebinar.topjava.model.User;
 import ru.javawebinar.topjava.to.UserTo;
-import ru.javawebinar.topjava.util.ValidationUtil;
-import ru.javawebinar.topjava.util.exception.IllegalRequestDataException;
 
 import javax.validation.Valid;
 import java.util.List;
